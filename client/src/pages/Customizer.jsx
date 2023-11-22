c9f579019aa6be0797ea8d95d80688607fcc6df1
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2023-11-22 18:42:13 +0100

    updated render link

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -58,7 +58,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('https://project-threejs-ai-tshirt.onrender.com/api/v1/dalle', {
+      const response = await fetch('https://project-threejs-ai-tshirt-netlify.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type' : 'application/json'
